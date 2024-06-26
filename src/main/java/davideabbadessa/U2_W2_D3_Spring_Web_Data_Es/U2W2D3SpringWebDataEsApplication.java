105dae9bce8e300894b2ee5c1f24a4fcf67216f6
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-26 13:12:36 +0200

    Initial commit

---
+++ src/main/java/davideabbadessa/U2_W2_D3_Spring_Web_Data_Es/U2W2D3SpringWebDataEsApplication.java
@@ -0,0 +1,13 @@
+package davideabbadessa.U2_W2_D3_Spring_Web_Data_Es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W2D3SpringWebDataEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W2D3SpringWebDataEsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davideabbadessa/U2_W2_D3_Spring_Web_Data_Es/U2W2D3SpringWebDataEsApplicationTests.java
@@ -0,0 +1,13 @@
+package davideabbadessa.U2_W2_D3_Spring_Web_Data_Es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W2D3SpringWebDataEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
